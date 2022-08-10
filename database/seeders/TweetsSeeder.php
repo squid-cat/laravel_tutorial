39fd1e7cde9560bcc6a48a28626a9c202875899a
squid-cat <kaikai120626@gmail.com>
2022-08-10 19:07:12 +0900

    tweetテーブルのmigrationとseeder作成

---
+++ database/seeders/TweetsSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use App\Models\Tweet;
+
+class TweetsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Tweet::factory()->count(10)->create();
+    }
+}
