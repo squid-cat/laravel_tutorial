a07a9cba44c9da2f7b2e477ffe370b51a495f1e3
squid-cat <kaikai120626@gmail.com>
2022-08-10 18:20:06 +0900

    sampleのコントローラーを作成

---
+++ app/Http/Controllers/Sample/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Sample;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function show(){
+        return 'Hello';
+    }
+    public function showId($id){
+        return "hello {$id}";
+    }
+}
