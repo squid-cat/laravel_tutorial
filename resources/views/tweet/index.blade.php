71f6510360fa0bc61a54c6587e1d2ca5bc4b59b5
squid-cat <kaikai120626@gmail.com>
2022-08-10 18:20:52 +0900

    --invokeを使用したコントローラーとbladeを使用したものを追加

---
+++ resources/views/tweet/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0,
+    maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Competible" content="ie=edge">
+    <title>つぶやきアプリ</title>
+  </head>
+  <body>
+    <h1>つぶやきアプリ</h1>
+    <p>{{ $name }}</p>
+  </body>
+</html>
+
